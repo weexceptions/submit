6b79f581b1706e1c0a60264cb7cd6146cb71323e
Sunny <Sunny@Sunny_HP>
2017-05-25 16:50:28 +0530

    New Upload Error free

--- DoctorAppointment/src/java/com/pro/controller/CreateUser.java
+++ DoctorAppointment/src/java/com/pro/controller/CreateUser.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.pro.controller;
+package com.pro.Controller;
 
 import com.pro.model.*;
 import com.pro.dao.UserDAO;
--- DoctorAppointment/src/java/com/pro/controller/MakeAppointment.java
+++ DoctorAppointment/src/java/com/pro/controller/MakeAppointment.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.pro.controller;
+package com.pro.Controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
